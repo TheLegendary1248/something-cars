3a860b57ef3caba523c763bb148da97e52fdf9ec
TheLegendary1248 <1248alex@gmail.com>
2022-08-17 11:08:13 -0400

    Car Driving now based on cursor pos

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -17,15 +17,19 @@ public class Car : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        transform.Rotate(transform.forward, turnSpeed * -Input.GetAxis("Horizontal") * Time.fixedDeltaTime);
-        rb.velocity += (Vector2)transform.up * Time.fixedDeltaTime * acceleration * Input.GetAxis("Vertical");
+        transform.Rotate(transform.forward, turnSpeed * -input.x * Time.fixedDeltaTime);
+        rb.velocity += (Vector2)transform.up * Time.fixedDeltaTime * acceleration * input.y;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector2 mousePos = (Input.mousePosition / Scr * 2f;
-        //mousePos
+        Vector2 centered = Input.mousePosition / new Vector2(Screen.width, Screen.height);
+        centered -= new Vector2(0.5f, 0.5f);
+        centered *= 4f;
+        centered.x = Mathf.Clamp(centered.x, -1f, 1f);
+        centered.y = Mathf.Clamp(centered.y, -1f, 1f);
+        input = centered;
     }
 }
